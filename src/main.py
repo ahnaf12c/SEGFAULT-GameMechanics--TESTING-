75f3b0da09027e6648336d44cc130711522dc527
Ahnaf101109 <148791555+ahnaf12c@users.noreply.github.com>
2026-09-04 01:22:27 +0600

    Add files via upload

---
+++ src/main.py
@@ -0,0 +1,19 @@
+import os
+import sys
+import time
+import assets
+import rooms
+import objects
+import GameEngine
+import world
+import player
+
+wmap = world.WORLD
+
+plr = player.Player()
+
+startLocation = '00'
+plr.location =  startLocation
+inv = plr.inventory
+
+
